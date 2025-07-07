36ed5a81659cc625bf6d0f4f7078388b45408703
egbadon-victor <thethinkerx100@gmail.com>
2025-07-07 12:08:40 +0100

    fixed

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -4,24 +4,22 @@ import Section from "../section/Section";
 
 import naijaLoaded from "../../assets/img/naijaLoaded.png";
 import pulseLogo from "../../assets/img/pulseLogo.png";
-import punchLogo from "../../assets/img/techPoint.png";
+import punchLogo from "../../assets/img/punchLogo.png";
 import vanguardLogo from "../../assets/img/vanguardLogo.png";
 import techPoint from "../../assets/img/techPoint.png";
 
 const News = () => {
-    return (
-        <>
-        <Section>
-                <div className={styles.newsWrapper}>
-                  <img src={punchLogo} alt="" className= {styles.punch} />
-                  <img src={naijaLoaded} alt="" className= {styles.naija} />
-                  <img src={pulseLogo} alt="" className= {styles.pulse} />
-                  <img src={vanguardLogo} alt="" className= {styles.vangua} />
-                  <img src={techPoint} alt="" className= {styles.tech}/>
-                </div>
-              </Section>
-        </>
-    )
-}
+  return (
+    <Section>
+      <div className={styles.newsWrapper}>
+        <img src={punchLogo} />
+        <img src={naijaLoaded} />
+        <img src={pulseLogo} />
+        <img src={vanguardLogo} />
+        <img src={techPoint} />
+      </div>
+    </Section>
+  );
+};
 
 export default News;
--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,39 +1,58 @@
 import styles from "./testimonial.module.css";
 import Section from "../section/Section";
-import Button from "../button/Button";
 
 import cards from "../../../src/assets/img/cards.webp";
 import sellCard from "../../../src/assets/img/sellCard.webp";
 import image from "../../../src/assets/img/image.png";
 import quote from "../../assets/img/quote.png";
 
-
 const Testimonial = () => {
+  const testimonials = [
+    {
+      name: "Ifeanyi Joseph",
+      store: "Play Store",
+      avatar: cards,
+      remark: "The withdrawal was quicker than I expected, keep it up guys.",
+    },
+    {
+      name: "Babalola Muiz",
+      store: "App Store",
+      avatar: image,
+      remark:
+        "I was able to buy a Sephora gift card within a minute. Quite impressive.",
+    },
+    {
+      name: "Nurudeen Abdulrazak",
+      store: "Play Store",
+      avatar: sellCard,
+      remark:
+        "I highly rate this app. I recommended it to my brothers and friends, and they also love it.",
+    },
+  ];
+
   return (
-    <>
-        <div className={styles.testimonialWrapper}>
-          <Section>
-            <h2 className={styles.heading}>
-            Testimonials <br />& News
-          </h2>
-          </Section>
-          
-          <div className={styles.cardGrid}>
-            {/* First Testimonial */}
+    <div className={styles.testimonialWrapper}>
+      <Section>
+        <h2 className={styles.heading}>
+          Testimonials <br />& News
+        </h2>
+      </Section>
+
+      <div className={styles.cardGrid}>
+        {testimonials.map((testimonial) => {
+          return (
             <div className={styles.card}>
               <img src={quote} alt="quotationmark" className={styles.quote} />
-              <p className={styles.text}>
-                The withdrawal was quicker than I expected, keep it up guys.
-              </p>
+              <p className={styles.text}>{testimonial.remark}</p>
               <div className={styles.footer}>
                 <div className={styles.sourceContainer}>
                   <div className={styles.sourceName}>
-                    <p className={styles.name}>Ifeanyi Joseph</p>
-                    <p className={styles.source}>Play Store</p>
+                    <p className={styles.name}>{testimonial.name}</p>
+                    <p className={styles.source}>{testimonial.store}</p>
                   </div>
                   <div>
                     <img
-                      src={cards}
+                      src={testimonial.avatar}
                       alt="IfeanyiJoseph"
                       className={styles.avatar}
                     />
@@ -41,55 +60,10 @@ const Testimonial = () => {
                 </div>
               </div>
             </div>
-
-            {/* Second Testimonial */}
-            <div className={styles.card}>
-            <img src={quote} alt="quotationmark" className={styles.quote} />
-              <p className={styles.text}>
-                I was able to buy a Sephora gift card within a minute. Quite
-                impressive.
-              </p>
-              <div className={styles.footer}>
-                <div className={styles.sourceContainer}>
-                  <div className={styles.sourceName}>
-                    <p className={styles.name}>Babalola Muiz</p>
-                    <p className={styles.source}>App Store</p>
-                  </div>
-                  <div>
-                    <img
-                      src={image}
-                      alt="Babalola Muiz"
-                      className={styles.avatar}
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            {/* Third Testimonial */}
-            <div className={styles.card}>
-            <img src={quote} alt="quotationmark" className={styles.quote} />
-              <p className={styles.text}>
-                I highly rate this app. I recommended it to my brothers and
-                friends, and they also love it.
-              </p>
-              <div className={styles.footer}>
-                <div className={styles.sourceContainer}>
-                  <div className={styles.sourceName}>
-                    <div className={styles.name}>Nurudeen Abdulrazak</div>
-                    <div className={styles.source}>Play Store</div>
-                  </div>
-                  <img
-                    src={sellCard}
-                    alt="Nurudeen Abdulrazak"
-                    className={styles.avatar}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-    </>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
